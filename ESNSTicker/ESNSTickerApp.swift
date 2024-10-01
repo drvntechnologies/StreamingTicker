55313980fcc2d55aad39542e9da71fdf7bc5eae6
Cameron Tarbell <cameron@drvnweb.com>
2024-09-30 20:49:41 -0500

    Initial Commit

---
+++ ESNSTicker/ESNSTickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESNSTickerApp.swift
+//  ESNSTicker
+//
+//  Created by Cameron Tarbell on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ESNSTickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ESNSTickerTests/ESNSTickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ESNSTickerTests.swift
+//  ESNSTickerTests
+//
+//  Created by Cameron Tarbell on 9/30/24.
+//
+
+import Testing
+@testable import ESNSTicker
+
+struct ESNSTickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
